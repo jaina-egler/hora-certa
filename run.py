cff94f7e4e2861cae1aaa6c41dee65cc1bdc8303
Jaina Cardoso <jainadadycardoso@gmail.com>
2026-05-16 14:54:32 -0400

    preparando projeto pra ser publicado

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 """
 Arquivo principal de execução da aplicação.
 
-Projeto: Sistema web para orientação e simulação de Atividades Complementares.
+Projeto: HoraCerta IFRO
 """
 
 from app import create_app
@@ -9,4 +9,4 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True)
